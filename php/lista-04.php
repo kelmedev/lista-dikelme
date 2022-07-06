2c9db9b76dfd237a0899258563a9cd94401d0b73
dikelme <kelmefhash@gmail.com>
2022-07-06 15:34:22 -0500

    confirmar em main

--- php/lista-04.php
+++ php/lista-04.php
@@ -1,18 +1,17 @@
 <?php
-
+    $num = 0;
+    //Questão 04
     $num = $_POST['num'];
-
+    
     echo "<h1> Resultado: <h1>";
-
-    if($num > 0) {
+    
+    if($num > 0){
         echo "<h2>O número digitado ($num) é positivo!<h2>";
     }
-        
-    elseif($num == 0) {
+        elseif($num == 0){
         echo "<h2>O número digitado ($num) é nulo!<h2>";
     }
-   
-    elseif($num < 0)  {
+    elseif($num < 0){
         echo "<h2>O número digitado ($num) é negativo!<h2>";
     }
 
