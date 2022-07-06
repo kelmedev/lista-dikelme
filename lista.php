14814bdd8b668bcf9ff3f5d4f6dd0a7b35fc2440
dikelme <kelmefhash@gmail.com>
2022-07-06 09:30:01 -0500

    confirmar em main

---
+++ lista.php
@@ -0,0 +1,14 @@
+<?php
+
+    $num = $_POST['num'];
+
+    $num > 0 //condição (estrutura de seleção)
+
+    if($num > 0) 
+        echo "positivo";
+
+    $num == 0 "nulo";
+
+    $num < 0 "negativo";
+
+?>
