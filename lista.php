a0588e179478a2f3cbd55f4e9d1d0e8df20d9d0a
dikelme <kelmefhash@gmail.com>
2022-07-06 10:29:55 -0500

    confirmar em main

--- lista.php
+++ lista.php
@@ -2,13 +2,23 @@
 
     $num = $_POST['num'];
 
-    $num > 0 //condição (estrutura de seleção)
+    $num > 0; //condição (estrutura de seleção)
 
-    if($num > 0) 
-        echo "positivo";
+    echo "<h1> Resultado: <h1>";
 
-    $num == 0 "nulo";
+    if($num > 0) {
+        echo "<h2>O número digitado ($num) é positivo!<h2>";
+    }
+        
+    elseif($num == 0) {
+        echo "<h2>O número digitado ($num) é nulo!<h2>";
+    }
+   
+    elseif($num < 0)  {
+        echo "<h2>O número digitado ($num) é negativo!<h2>";
+    }
 
-    $num < 0 "negativo";
+    
+   
 
 ?>
